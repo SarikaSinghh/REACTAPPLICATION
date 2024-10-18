c9432b1f11ca5e04865ce729ed9d62081306c443
Sarika Singhh <spriya87198@gmail.com>
2024-10-18 14:43:03 +0530

    first commit

---
+++ reactapplication/src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    
+<body>
+   
+    <h1>Hello Everyone!</h1><br></br>
+    <p>
+    <h4>This is my Portfolio website</h4>
+    </p>
+    
+   
+      
+        <section> Skills : <a href="skills.html">My Skills</a></section><br/><br/>
+        <section> Education : <a href="Education.html">My Education</a></section><br/>
+        <section>Projects : <a href="projects.html"> My Projects</a></section><br/>
+
+
+       
+
+</body>
+      
+    
+
+
+
+
+
+
+    </>
+  )
+}
+
+export default App
